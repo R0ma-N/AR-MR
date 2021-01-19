1df9cb8f3e86feee5a432f1d417c6fc632b6cb5a
R0ma-N <x6wert@gmail.com>
2021-01-19 17:03:26 +0300

    Ground Plane & Mid Air

---
+++ Assets/Scripts/SceneMngr.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneMngr : MonoBehaviour
+{
+    public void LoadGroundMode()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+    public void LoadMidAirMode()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+    }
+}
